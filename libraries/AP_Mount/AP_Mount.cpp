751a8a3950c0ec08ea13bbb9202b1b6ee1f0f725
Siddharth Bharat Purohit <siddharthbharatpurohit@gmail.com>
2015-10-20 20:14:46 -0700

    AP_Mount: fix bug causing registering gimbal to accelcal before its initialised

--- libraries/AP_Mount/AP_Mount.cpp
+++ libraries/AP_Mount/AP_Mount.cpp
@@ -10,6 +10,7 @@
 #include "AP_Mount_Alexmos.h"
 #include "AP_Mount_SToRM32.h"
 #include "AP_Mount_SToRM32_serial.h"
+#include <AP_AccelCal/AP_AccelCal.h>
 
 const AP_Param::GroupInfo AP_Mount::var_info[] PROGMEM = {
     // @Param: _DEFLT_MODE
@@ -402,7 +403,7 @@ AP_Mount::AP_Mount(const AP_AHRS_TYPE &ahrs, const struct Location &current_loc)
 }
 
 // init - detect and initialise all mounts
-void AP_Mount::init(DataFlash_Class *dataflash, const AP_SerialManager& serial_manager)
+void AP_Mount::init(DataFlash_Class *dataflash, const AP_SerialManager& serial_manager, AP_AccelCal &acal)
 {
     // check init has not been called before
     if (_num_instances != 0) {
@@ -419,7 +420,6 @@ void AP_Mount::init(DataFlash_Class *dataflash, const AP_SerialManager& serial_m
     }
 
     _dataflash = dataflash;
-    
     // primary is reset to the first instantiated mount
     bool primary_set = false;
 
@@ -438,7 +438,7 @@ void AP_Mount::init(DataFlash_Class *dataflash, const AP_SerialManager& serial_m
 #if AP_AHRS_NAVEKF_AVAILABLE
         // check for MAVLink mounts
         } else if (mount_type == Mount_Type_MAVLink) {
-            _backends[instance] = new AP_Mount_MAVLink(*this, state[instance], instance);
+            _backends[instance] = new AP_Mount_MAVLink(*this, state[instance], instance, acal);
             _num_instances++;
 #endif
 
