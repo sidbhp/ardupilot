8de34890bb4c68a230fc80384108fbb2b187045f
Siddharth Bharat Purohit <siddharthbharatpurohit@gmail.com>
2016-03-26 22:05:07 -0700

    AP_GST: create globally shared topics library

---
+++ libraries/AP_GST/AP_GST.cpp
@@ -0,0 +1,120 @@
+/*
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <stdlib.h>
+//TODO:
+// * support for multiple read and write
+// * support for strerr
+// * ability to lock memory
+int8_t AP_GST::open(struct gst_metadata *meta, enum open_type fdt)
+{
+    struct rdwr_info rdwr;
+    switch(fdt) {
+        case O_WRITE: {
+            //Create node
+            write_node *new_node;
+            new_node = new(write_node);
+            new_node->meta = meta;
+            new_node->data = malloc(meta->o_size);
+
+            //Store node
+            write_list.push_back(new_node);
+
+            //update fd list
+            rdwr.fd = last_write_fd;
+            rdwr.fdtype = O_WRITE;
+            rdwr_info_list.push_back(rdwr);
+            last_write_fd++;
+            return last_rdwr_fd++;
+        }
+        case O_READ: {
+            //Find appropriate write node
+            read_node *new_node;
+            int8_t write_fd = -1;            
+            for(uint8_t i=0; i<last_write_fd; i++) {
+                if(strcmp(meta->o_name, write_list[i]->meta->o_name) == 0) {
+                    write_fd = i;
+                    break;
+                }
+            }
+
+            //Create read node
+            if(write_fd != -1) {
+                new_node = new(read_node);
+                new_node->meta = meta;
+                new_node->fd = write_fd;
+                read_list.push_back(new_node);
+            } else {
+                return -1;
+            }
+
+            //update fd list
+            rdwr.fd = last_read_fd;
+            rdwr.fdtype = O_READ;
+            rdwr_info_list.push_back(rdwr);
+            last_read_fd++;
+            return last_rdwr_fd++;
+        }
+        default:
+        return -1;
+    }
+}
+
+int8_t AP_GST::write(int8_t fd, const void* data)
+{
+    if(fd > last_rdwr_fd || rdwr_info_list[fd].fd_type != O_WRITE) {
+        return -1;
+    }
+
+    //convert rdwr fd to write fd
+    fd = rdwr_info_list[last_rdwr_fd];
+    //write data to memory
+    memcpy(write_list[fd]->data, data,  write_list[fd]->meta->o_size);
+    //raise updated flags of relevant read nodes
+    for(uint8_t i = 0; i < last_read_fd; i++) {
+        if(strcmp(read_list[fd]->meta->o_name,write_list[fd]->meta->o_name)) {
+            read_list[fd]->updated = 0;
+        }
+    }
+    return 0;
+}
+
+int8_t AP_GST::read(int8_t fd, void* data)
+{
+    if(fd > last_rdwr_fd || rdwr_info_list[fd].fd_type != O_READ) {
+        return -1;
+    }
+    //convert rdwr fd to read fd
+    fd = rdwr_info_list[last_rdwr_fd];
+    int8_t write_fd = read_list[fd]->fd;
+    //check for fd sanity
+    if(strcmp(read_list[fd]->meta->o_name,write_list[write_fd]->meta->o_name) == 0) {
+        return -1;
+    }
+    //write data to memory
+    memcpy(data, write_list[write_fd]->data, write_list[write_fd]->meta->o_size);
+    read_list[fd]->updated = -1;
+    return 0;
+}
+
+int8_t AP_GST::available(int8_t fd)
+{
+    if(fd > last_rdwr_fd || rdwr_info_list[last_rdwr_fd].fd_type != O_READ) {
+        return -1;
+    }
+    //convert rdwr fd to read fd
+    fd = rdwr_info_list[last_rdwr_fd];
+    return read_list[fd]->updated;
+}
---
+++ libraries/AP_GST/AP_GST.h
@@ -0,0 +1,85 @@
+/*
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/* Globally Shared Topics - Inspired by uORB on PX4 */
+
+#include <AP_GST_Helper.h>
+#include <vector>
+#include "topics/sensor_mag.h"
+#include "topics/sensor_accel.h"
+#include "topics/sensor_gyro.h"
+#include "topics/sensor_baro.h"
+#include "topics/output_pwm.h"
+#include "topics/input_rc.h"
+#include "topics/pwm_input.h"
+#include "topics/vehicle_attitude.h"
+
+struct gst_metadata {
+	const char *o_name;		/**< unique object name */
+	const size_t o_size;		/**< object size */
+};
+
+GST_DEFINE(sensor_mag, struct sensor_mag_s);
+GST_DEFINE(sensor_accel0, struct sensor_accel_s);
+GST_DEFINE(sensor_accel1, struct sensor_accel_s);
+GST_DEFINE(sensor_accel2, struct sensor_accel_s);
+GST_DEFINE(sensor_gyro0, struct sensor_gyro_s);
+GST_DEFINE(sensor_gyro1, struct sensor_gyro_s);
+GST_DEFINE(sensor_gyro2, struct sensor_gyro_s);
+GST_DEFINE(sensor_baro, struct sensor_baro_s);
+GST_DEFINE(output_pwm, struct output_pwm_s);
+GST_DEFINE(input_rc, struct input_rc_s);
+GST_DEFINE(pwm_input, struct pwm_input_s);
+GST_DEFINE(vehicle_attitude, struct vehicle_attitude_s);
+
+
+enum open_type {
+    O_READ,
+    O_WRITE,
+};
+
+class AP_GST {
+
+    AP_GST() : last_fd(0) {}
+    int8_t open(struct gst_metadata *meta, enum open_type fdt);
+    int8_t write(int8_t fd, const void* data);
+    int8_t read(int8_t fd, void *buffer);
+    int8_t available(int8_t fd);
+private:
+
+    struct write_node {
+        struct gst_metadata* meta;
+        void* data;
+    };
+
+    struct read_node {
+        struct gst_metadata* meta;
+        int8_t write_fd;
+        int8_t updated;
+    };
+
+    struct rdwr_info {
+        int8_t fd;
+        open_type fd_type;
+    };
+    
+    std::vector<write_node*> write_list;
+    std::vector<read_node*> read_list;
+    std::vector<rdwr_info> rdwr_info_list;
+ 
+    int8_t last_write_fd;
+    int8_t last_read_fd;
+    int8_t last_rdwr_fd;
+};
