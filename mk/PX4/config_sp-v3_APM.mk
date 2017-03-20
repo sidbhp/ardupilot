75a8b2b500151618f59c195bf372879b52ec8f5e
Siddharth Bharat Purohit <siddharthbharatpurohit@gmail.com>
2017-03-20 23:48:28 +0530

    AP_BoardConfig: start lsm303d and l2gd20 drivers for lsm9ds0

--- mk/PX4/config_sp-v3_APM.mk
+++ mk/PX4/config_sp-v3_APM.mk
@@ -3,8 +3,8 @@
 #
 include $(SKETCHBOOK)/mk/PX4/px4_common.mk
 
-#MODULES		+= drivers/lsm303d
-#MODULES		+= drivers/l3gd20
+MODULES		+= drivers/lsm303d
+MODULES		+= drivers/l3gd20
 MODULES		+= drivers/mpu9250
 MODULES		+= drivers/boards/sp-v3
 MODULES		+= drivers/pwm_input
