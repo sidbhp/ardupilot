9602d040d84f042ec9bc8e39800d4033a725ec20
Siddharth Purohit <siddharthbharatpurohit@gmail.com>
2018-10-20 09:54:06 +0800

    AP_UAVCAN: add support for flow and range sensor message over can

--- libraries/AP_OpticalFlow/OpticalFlow.cpp
+++ libraries/AP_OpticalFlow/OpticalFlow.cpp
@@ -131,6 +131,8 @@ void OpticalFlow::update(void)
 
     if (backend != nullptr) {
         backend->update();
+    } else if (backend == nullptr) {
+        init();
     }
 
     // only healthy if the data is less than 0.5s old
